208a44677c9be701cfdcdd8b948178a2dac36e68
Ilia-Ivankov <ilaivankov919@gmail.com>
2025-03-16 19:09:19 +0300

    add changes in databases

--- database.sql
+++ database.sql
@@ -1,17 +1,16 @@
 -- Таблица для хранения URL-адресов.
 CREATE TABLE urls (
-    id SERIAL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Таблица для хранения данных URL.
 CREATE TABLE url_data (
-    id SERIAL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     response_code INTEGER,
     h1 VARCHAR(255),
     title VARCHAR(255),
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
 
